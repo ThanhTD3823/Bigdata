24c77333a6d2ef9467b0f76fdd535436a7447179
ThanhTD3823 <trandangthanh3823@gmail.com>
2024-10-05 09:20:10 +0700

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -1,8 +1,18 @@
-/**
- * 
- */
-/**
- * 
- */
-module BKT {
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class ElectricityMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        // Tách từng dòng dữ liệu
+        String[] fields = value.toString().split(" ");
+        String year = fields[0]; // Năm
+        int avgElectricity = Integer.parseInt(fields[fields.length - 1]); // Lấy giá trị trung bình
+
+        // Đưa cặp (year, avgElectricity) vào context
+        context.write(new Text(year), new IntWritable(avgElectricity));
+    }
 }
