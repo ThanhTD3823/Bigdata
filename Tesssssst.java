364255907f53ffa1cdadec333b1cfd77339676b3
ThanhTD3823 <trandangthanh3823@gmail.com>
2024-11-09 09:28:20 +0700

    Update Tesssssst.java

--- Tesssssst.java
+++ Tesssssst.java
@@ -1,1 +1,15 @@
-https://drive.google.com/file/d/18cST8tqEIimDDmtiv_zTHQsvbPMhcOb7/view?usp=sharing
+CREATE TABLE netflix_movies_raw (
+  title STRING,
+  director STRING,
+  casts STRING,
+  country STRING,
+  date_added STRING,
+  release_year INT,
+  rating STRING,
+  duration STRING,
+  listed_in STRING,
+  description STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE;
