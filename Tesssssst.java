bc59d470870e85046f80317e1b97d8b47c470e5e
ThanhTD3823 <trandangthanh3823@gmail.com>
2024-11-09 09:50:59 +0700

    Update Tesssssst.java

--- Tesssssst.java
+++ Tesssssst.java
@@ -1,15 +1,3 @@
-CREATE TABLE netflix_movies_raw (
-  title STRING,
-  director STRING,
-  casts STRING,
-  country STRING,
-  date_added STRING,
-  release_year INT,
-  rating STRING,
-  duration STRING,
-  listed_in STRING,
-  description STRING
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+CREATE TABLE netflix_movies_raw (title STRING, director STRING, casts STRING, country STRING, date_added STRING, release_year INT, rating STRING, duration STRING, listed_in STRING, description STRING)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE;
